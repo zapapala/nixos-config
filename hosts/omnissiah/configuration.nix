3051d3f4b9fab33e31e240c0c1516143c6ba3fbe
OC Blanco <zapapala@qpost.top>
2025-11-20 13:08:40 +0100

    Update configuration.nix

--- hosts/omnissiah/configuration.nix
+++ hosts/omnissiah/configuration.nix
@@ -60,8 +60,12 @@
 
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  #services.xserver.displayManager.gdm.enable = true;
+  #services.xserver.desktopManager.gnome.enable = true;
+
+  # Enable the COSMIC Desktop Environment
+  services.displayManager.cosmic-greeter.enable = true;
+  services.desktopManager.cosmic.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
