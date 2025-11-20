f44e54d809f6edee260b4572b2e3254830b2c99f
OC Blanco <zapapala@qpost.top>
2025-11-20 14:13:05 +0100

    Update stabledavinci.nix

--- modules/stabledavinci.nix
+++ modules/stabledavinci.nix
@@ -1,5 +1,9 @@
 { config, pkgs, nixpkgsStable, ... }:
 {
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # Overlay
   nixpkgs.overlays = [
     (final: prev: {
       davinci-resolve = nixpkgsStable.legacyPackages.x86_64-linux.davinci-resolve;
