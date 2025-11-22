1bd5ebdfaf5f3520774f361f653d56e67224d737
OC Blanco <zapapala@qpost.top>
2025-11-22 16:23:52 +0100

    Added FW13 hardware module

--- flake.nix
+++ flake.nix
@@ -2,16 +2,18 @@
   description = "OC Blanco Flake";
 
   inputs = {
-    # NixOS official package source, using the nixos-25.05 branch here
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     nixpkgs-stable.url = "github:NixOS/nixpkgs/25.05";
+    nixos-hardware.url = "github:NixOS/nixos-hardware/master";
   };
 
-  outputs = { self, nixpkgs, nixpkgs-stable, ... } @ inputs:
+  outputs = { self, nixpkgs, nixpkgs-stable, nixos-hardware, ... } @ inputs:
     let
       lib = nixpkgs.lib;
       system = "x86_64-linux";
     in {
+
+    # Omnissiah desktop
     nixosConfigurations = {
       omnissiah = lib.nixosSystem {
         inherit system;
@@ -42,12 +44,35 @@
         ];
       };
     };
+
+    # Skitarii laptop - Framework 13 7040
       nixosConfigurations = {
         skitarii = lib.nixosSystem {
           inherit system;
+          specialArgs = { inherit inputs; };
+
+
           modules = [
+            ({
+              nixpkgs.overlays = [
+                (final: prev: {
+                  stable = import nixpkgs-stable {
+                    inherit system;
+                    config = {
+                      allowUnfree = true;
+                      permittedInsecurePackages = [
+                        "ventoy-qt5-1.1.05"
+                        "qtwebengine-5.15.19"
+                      ];
+                    };
+                  };
+                })
+              ];
+            })
+
             ./hosts/skitarii/configuration.nix
             ./hosts/skitarii/packages.nix
+            nixos-hardware.nixosModules.framework-13-7040-amd
           ];
        };
     };
