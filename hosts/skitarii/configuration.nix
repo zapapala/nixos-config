fecc71c8b815dca5bdbf899351a352780b08a15e
OC Blanco <zapapala@qpost.top>
2025-11-22 16:43:08 +0100

    fixed hostname for Skitarii

--- hosts/skitarii/configuration.nix
+++ hosts/skitarii/configuration.nix
@@ -18,7 +18,7 @@
   # Use latest kernel.
   boot.kernelPackages = pkgs.linuxPackages_latest;
 
-  networking.hostName = "skitari"; # Define your hostname.
+  networking.hostName = "skitarii"; # Define your hostname.
   # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # Configure network proxy if necessary
